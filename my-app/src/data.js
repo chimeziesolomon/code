380f4b8696aebdb3503bb17ec1a662bc2e36a868
Chimezie Solomon <chimezie.solomon.cs@gmail.com>
2023-03-17 11:55:29 +0100

    initial commit

---
+++ my-app/src/data.js
@@ -0,0 +1,62 @@
+export default [
+    {
+        id: 1,
+        name: 'Victor Guy',
+        age: 27,
+        image: 'https://res.cloudinary.com/chimeziesolomon/image/upload/v1679047153/aa_lzqein.jpg',
+    },
+    {
+        id: 2,
+        name: 'Slimmy girl',
+        age: 26,
+        Image: 'https://res.cloudinary.com/chimeziesolomon/image/upload/v1679047139/a1_npdzyh.jpg',
+    },
+    {
+        id: 3,
+        name: 'sure Babe',
+        age: 37,
+        Image: 'https://res.cloudinary.com/chimeziesolomon/image/upload/v1679047126/a2_ueu6bl.jpg',
+    },
+    {
+        id: 4,
+        name: 'Enjoyment Girl',
+        age: 17,
+        Image: 'https://res.cloudinary.com/chimeziesolomon/image/upload/v1679047114/a3_kjdklg.jpg',
+    },
+    {
+        id: 5,
+        name: 'Victorious Babe',
+        age: 23,
+        Image: 'https://res.cloudinary.com/chimeziesolomon/image/upload/v1679047107/a4_im287o.jpg',
+    },
+    {
+        id: 6,
+        name: 'Victor Guy',
+        age: 21,
+        Image: 'https://res.cloudinary.com/chimeziesolomon/image/upload/v1679047091/b1_codafs.jpg',
+    },
+    {
+        id: 7,
+        name: ' Guy Man',
+        age: 27,
+        Image: 'https://res.cloudinary.com/chimeziesolomon/image/upload/v1679047074/b2_afhzu0.jpg',
+    },
+    {
+        id: 8,
+        name: 'Sure Man',
+        age: 20,
+        Image: 'https://res.cloudinary.com/chimeziesolomon/image/upload/v1679047061/b3_ypq978.jpg',
+    },
+    {
+        id: 9,
+        name: 'Myron Guy',
+        age: 29,
+        Image: 'https://res.cloudinary.com/chimeziesolomon/image/upload/v1679047050/b4_xyfcgs.jpg',
+    },
+    {
+        id: 10,
+        name: 'Odogwu',
+        age: 30,
+        image: 'https://res.cloudinary.com/chimeziesolomon/image/upload/v1679047036/b5_cjfino.jpg',
+    },
+];
